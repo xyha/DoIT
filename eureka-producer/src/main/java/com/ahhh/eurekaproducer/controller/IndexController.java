abbf0bfc11748848695f5b139d161c8c7b41b5e1
xueyh <xueyh@nysoftland.com.cn>
2020-12-15 11:13:14 +0800

    测试负载均衡 返回加上端口

--- eureka-producer/src/main/java/com/ahhh/eurekaproducer/controller/IndexController.java
+++ eureka-producer/src/main/java/com/ahhh/eurekaproducer/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.ahhh.eurekaproducer.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class IndexController {
-
+    @Value("${server.port}")
+    private Integer port;
     @GetMapping("/index")
     public String index(String name){
-        return "eureka:" + name;
+        return "eureka:" + name + "  port:" + port;
     }
 }
